e57231983a82c5b9b0892dc072eeca169137fb65
Otmaromiguel <otmaromiguel_@hotmail.com>
2016-04-26 18:56:24 -0600

    los valores ya se leen
    Hice una nueva estructura para el infrarojo,y el puerto serial cuando el
    valor es mayor de 100 = 1 y cuando es menor a 100 es = 0

--- CarritoseguidorleanealITR/CarritoseguidorleanealITR.ino
+++ CarritoseguidorleanealITR/CarritoseguidorleanealITR.ino
@@ -1,76 +1,36 @@
-int derpin1 = 2;
-int derpin2 = 3;
-int izqpin1 =  4;
-int izqpin2 = 5;
-int izqinfra = 6;
-int derinfra = 7;
-int ceninfra = 8;
-int Valorinfra = 0;
+int Dermotor[2] = {3, 6};
+int Izqmotor[2] = {9, 11};
+int infrarojos[3] = {A0, A1, A2};
+float Estadosinf[3] = {0, 0, 0};
+int Estadosen[3] = {0, 0, 0};
 void setup() {
-  pinMode(derpin1,OUTPUT);
-  pinMode(derpin2,OUTPUT);
-  pinMode(izqpin1,OUTPUT);
-  pinMode(izqpin2,OUTPUT);
-  pinMode(izqinfra,INPUT);
-  pinMode(derinfra,INPUT);
-  pinMode(ceninfra,INPUT);
-
-
+  Serial.begin(9600);
+  for (int i = 0; i < 2 ; i++) {
+    pinMode(Dermotor[i], OUTPUT);
+    pinMode(Izqmotor[i], OUTPUT);
+  }
+  for (int i = 0; i < 3 ; i++) {
+    pinMode(infrarojos[i], INPUT);
+  }
 }
 
 void loop() {
-  if (Valorinfra = digitalRead(ceninfra)){
-    
-  digitalWrite(derpin1,Valorinfra);
-  digitalWrite(izqpin2,Valorinfra);
+  for (int i = 0; i < 3 ; i++) {
+    Estadosinf[i] = analogRead(infrarojos[i]);
+    if (Estadosinf[i] > 100) {
+      Estadosen[i] = 1;
+    }
+    else {
+      Estadosen[i] = 0;
+    }
+    Serial.print(" S");
+    Serial.print(i + 1);
+    Serial.print(" ");
+    Serial.print(Estadosen[i]);
 
   }
-  else {
-    digitalWrite(derpin1,LOW);
-  digitalWrite(derpin2,LOW);
-  digitalWrite(izqpin1,LOW);
-  digitalWrite(izqpin2,LOW);
-  }
+  Serial.println();
+  
 
-
-   
- if (Valorinfra = digitalRead(izqinfra)) {
-   digitalWrite(izqpin1,Valorinfra);
-   digitalWrite(izqpin2,Valorinfra);
-   digitalWrite(derpin1,LOW);
-   digitalWrite(derpin2,LOW);
- }
-  else {
-    digitalWrite(derpin1,LOW);
-  digitalWrite(derpin2,LOW);
-  digitalWrite(izqpin1,LOW);
-  digitalWrite(izqpin2,LOW);
-  }
-
-if(Valorinfra = digitalRead(derinfra)){
-  digitalWrite(derpin1,Valorinfra);
-  digitalWrite(derpin2,Valorinfra);
-  digitalWrite(izqpin1,LOW);
-  digitalWrite(izqpin2,LOW);
-}
- else {
-    digitalWrite(derpin1,LOW);
-  digitalWrite(derpin2,LOW);
-  digitalWrite(izqpin1,LOW);
-  digitalWrite(izqpin2,LOW);
-  }
-if(Valorinfra = !Valorinfra){
-digitalWrite(derpin1,LOW);
-  digitalWrite(derpin2,LOW);
-  digitalWrite(izqpin1,LOW);
-  digitalWrite(izqpin2,LOW);
-}
- else {
-    digitalWrite(derpin1,LOW);
-  digitalWrite(derpin2,LOW);
-  digitalWrite(izqpin1,LOW);
-  digitalWrite(izqpin2,LOW);
-  }
-
-
+  delay(1000);
 }
